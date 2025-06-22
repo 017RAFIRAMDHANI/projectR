acdb81397fc9de2baec52ea8d06cc1cb97e04b26
017RAFIRAMDHANI <rafiramdhani1122@gmail.com>
2025-06-22 13:56:41 +0700

    master

---
+++ app/Models/Employe.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employe extends Model
+{
+    use HasFactory;
+   protected $table = 'employes';
+    protected $primaryKey = 'id_employe';
+      protected $fillable = [
+         'name',
+         'company_name',
+         'position',
+         'type',
+         'file_card',
+         'status',
+      ];
+}
---
+++ database/migrations/2025_06_22_064440_create_employes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employes', function (Blueprint $table) {
+          $table->increments('id_employe');  // Primary key
+            $table->string('name')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('position')->nullable();
+            $table->string('type')->nullable();
+            $table->string('file_card')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employes');
+    }
+};
