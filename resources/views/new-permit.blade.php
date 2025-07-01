00d28cf105e476c2c66a745439aa77a0248494b3
017RAFIRAMDHANI <rafiramdhani1122@gmail.com>
2025-07-01 20:18:58 +0700

    master

--- resources/views/new-permit.blade.php
+++ resources/views/new-permit.blade.php
@@ -197,7 +197,7 @@
                         <input type="tel" name="pic_contact" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500">
           </div>
               <div>
-                        <label class="block text-sm font-medium text-gray-700 mb-2">Car Plate No</label>
+                        <label class="block text-sm font-medium text-gray-700 mb-2">Car Plate No. <b>(A 1234 ABC)</b></label>
                         <input type="text" name="car_plate_no" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500">
               </div>
               <div>
@@ -286,7 +286,7 @@
                 <label class="block text-sm font-medium text-gray-700 mb-2">Company Name</label>
                 <input type="text" name="company_name" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500" required>
             </div>
-              
+
               <div>
                 <label class="block text-sm font-medium text-gray-700 mb-2">Urgency</label>
                 <select name="mode" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500">
@@ -495,11 +495,11 @@
             <h3 class="text-lg font-semibold text-gray-800 mb-4">Vehicle Information</h3>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div>
-                <label class="block text-sm font-medium text-gray-700 mb-2">Car Plate No.</label>
+                <label class="block text-sm font-medium text-gray-700 mb-2">Car Plate No. <b>(A 1234 ABC)</b></label>
                 <input type="text" name="number_plate" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500">
           </div>
               <div>
-                <label class="block text-sm font-medium text-gray-700 mb-2">Vehicle Type</label>
+                <label class="block text-sm font-medium text-gray-700 mb-2">Vehicle Type </label>
                 <select name="vehicle_types" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500">
                   <option value="">Select Vehicle Type</option>
                   <option value="Car">Car</option>
